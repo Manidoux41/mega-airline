a758676180004f001908778c2629e5553e66ed57
superflyman90 <superflyman90@gmail.com>
2022-11-02 06:55:41 +0100

    stylisation du header

--- src/components/Public/Header.jsx
+++ src/components/Public/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header>
-        <h1>Mega AIrline</h1>
+        <h1>Mega Airline</h1>
         <nav>
           <ul>
             <li>
